bc31031bfd3d2ae8a3dad549866ce0147c247bd2
Astro Code <112213367+IDE-Programming@users.noreply.github.com>
2024-03-23 22:10:33 -0500

    Add files via upload

---
+++ Modulo.js
@@ -0,0 +1,14 @@
+var ListaSedes = ["Seleccione","Pomasqui"];
+var ListadoDeSedes = ["Seleccione","Todas","Pomasqui"];
+
+
+//   CREDENCIALES DE PRUEBA
+var apiKey = "AIzaSyDQeViHZk_m3XJdgoAHzpIoaodlNwTUsDk";
+var authDomain = "home-service-cf711.firebaseapp.com";
+var databaseURL= "https://home-service-cf711-default-rtdb.firebaseio.com";
+var projectId= "home-service-cf711";
+var storageBucket= "home-service-cf711.appspot.com";
+var messagingSenderId= "876118499149";
+var appId= "1:876118499149:web:6cfea6aba9b438cfbda104";
+var  measurementId= "G-06RPS8CMS2";
+
